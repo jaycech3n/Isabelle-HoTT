25225e0c637a43319fef6889dabc222df05bfd3c
Josh Chen <jaycech3n@gmail.com>
2018-08-08 10:35:43 +0200

    Rewriting projection functions

--- HoTT_Methods.thy
+++ HoTT_Methods.thy
@@ -57,9 +57,9 @@ method derive uses lems unfolds = (
   unfold unfolds |
   simple lems: lems |
   match lems in lem: "?X : ?Y" \<Rightarrow> \<open>wellformed jdgmt: lem\<close> |
-  rule Universe_hierarchy |
-  (rule Universe_cumulative, simple lems: lems) |
-  (rule Universe_cumulative, match lems in lem: "?X : ?Y" \<Rightarrow> \<open>wellformed jdgmt: lem\<close>)
+  rule U_hierarchy |
+  (rule U_cumulative, simple lems: lems) |
+  (rule U_cumulative, match lems in lem: "?X : ?Y" \<Rightarrow> \<open>wellformed jdgmt: lem\<close>)
   )+
 
 
